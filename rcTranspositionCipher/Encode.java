3491b026da34aa5626f77ac52f0f0420473793a9
Regina Chen <rchen41@stuy.edu>
2024-05-28 17:52:33 -0400

    encode msg works

--- rcTranspositionCipher/Encode.java
+++ rcTranspositionCipher/Encode.java
@@ -2,12 +2,17 @@ public class Encode {
 
   public static void main(String[] args) {
     String message = "hello world";
+    String key = "3241";
 
-    String encodeMsg = encode(message, 3, 4);
+    String encodeMsg = encode(message, key);
+    System.out.println(encodeMsg);
 
   }
 
-  public static String encode(String msg, int row, int col) {
+  public static String encode(String msg, String key) {
+    int col = key.length();
+    int row = (msg.length() / col);
+    if (row * col < msg.length()) row++;
     String encodeMsg = "";
     char[][] ary = new char[row][col];
 
@@ -24,12 +29,16 @@ public class Encode {
     char alphabet = 'A' - 1;
     while (c < col) {
       ary[r][c++] = (char)(((alphabet - 'A' + 1) % 26) + 'A');
+      alphabet++;
     }
 
     printAry(ary);
 
-    r = 0;
-    c = 0;
+    for (int i=0; i<col; i++) {
+      r = 0;
+      int keyc = key.charAt(i) - '0';
+      while (r < row) encodeMsg += ary[r++][keyc-1];
+    }
 
     return encodeMsg;
   }
