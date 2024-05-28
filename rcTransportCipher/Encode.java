535860c4d164d396ece7c013d1eb3a79f68a001f
Regina Chen <rchen41@stuy.edu>
2024-05-28 15:32:08 -0400

    adding random letters

--- rcTransportCipher/Encode.java
+++ rcTransportCipher/Encode.java
@@ -8,22 +8,39 @@ public class Encode {
   }
 
   public static String encode(String msg, int row, int col) {
+    String encodeMsg = "";
     char[][] ary = new char[row][col];
+
     int r = 0;
     int c = 0;
     for (int i=0; i<msg.length(); i++) {
-      char[r][c] = msg.charAt(i);
-      c = (c + 1) % 4;
-      if (c == 0) r++;
+      if (msg.charAt(i) != ' ') {
+        ary[r][c] = Character.toUpperCase(msg.charAt(i));
+        c = (c + 1) % 4;
+        if (c == 0) r++;
+      }
     }
+
+    char alphabet = 'A' - 1;
+    while (c < col) {
+      ary[r][c++] = (char)(((alphabet - 'A' + 1) % 26) + 'A');
+    }
+
+    printAry(ary);
+
     r = 0;
     c = 0;
 
-
+    return encodeMsg;
   }
 
   public static void printAry(char[][] ary) {
-    
+    for (int r=0; r<ary.length; r++) {
+      for (int c=0; c<ary[0].length; c++) {
+        System.out.print(ary[r][c] + " ");
+      }
+      System.out.println("");
+    }
   }
 
 }
